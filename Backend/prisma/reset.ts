5db8c5893c11cc281338f6badc5953fdf7ec813a
MattRH7 <mhadiwidjaja@student.ciputra.ac.id>
2025-01-04 17:48:41 +0700

    Create reset.ts
    You can reset the database using "npx ts-node prisma/reset.ts"

---
+++ Backend/prisma/reset.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.$executeRaw`DROP SCHEMA public CASCADE;`;
+  await prisma.$executeRaw`CREATE SCHEMA public;`;
+
+  console.log("Database reset successfully");
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
